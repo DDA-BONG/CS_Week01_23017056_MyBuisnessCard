60ce2bc67fd1016cc16e6815084fc8c3b2362421
윤형규 <yhk1071206@gmail.com>
2026-03-05 19:24:21 +0900

    사진에 마우스를 올리면 테두리 변화, 종료버튼 추

--- Form1.cs
+++ Form1.cs
@@ -6,7 +6,8 @@ namespace WinFormsApp3
         public Form1()
         {
             InitializeComponent();
-
+            pictureBox1.MouseEnter += PictureBox1_MouseEnter;
+            pictureBox1.MouseLeave += PictureBox1_MouseLeave;
         }
 
         private void Form1_Load(object sender, EventArgs e)
