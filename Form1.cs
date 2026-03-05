dbc139dae755f021fc4afab37f1644e06ed8ed4f
윤형규 <yhk1071206@gmail.com>
2026-03-05 18:29:45 +0900

    Revert "1. 사진에 마우스를 올리면 테두리 변화"
    This reverts commit 586a6d3766211ad7f71f96b591d25b092ca943ed.

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,3 @@
-
 namespace WinFormsApp3
 {
     public partial class Form1 : Form
@@ -6,10 +5,6 @@ namespace WinFormsApp3
         public Form1()
         {
             InitializeComponent();
-
-            // pictureBox1에 마우스가 올라가거나 나갈 때 테두리 변경
-            pictureBox1.MouseEnter += PictureBox1_MouseEnter;
-            pictureBox1.MouseLeave += PictureBox1_MouseLeave;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -26,17 +21,7 @@ namespace WinFormsApp3
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            // 클릭 시 동작이 필요하면 여기에 추가
-        }
 
-        private void PictureBox1_MouseEnter(object sender, EventArgs e)
-        {
-            pictureBox1.BorderStyle = BorderStyle.Fixed3D; // 마우스 올리면 테두리 변화
-        }
-
-        private void PictureBox1_MouseLeave(object sender, EventArgs e)
-        {
-            pictureBox1.BorderStyle = BorderStyle.None; // 마우스 나가면 원래 테두리로 복원
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -67,11 +52,6 @@ namespace WinFormsApp3
                 UseShellExecute = true
             });
 
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            pictureBox1.Image = Properties.Resources.wp5271636_seven_movie_wallpapers; // 버튼 클릭 시 이미지 변경
         }
     }
 }
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -69,15 +69,5 @@ namespace WinFormsApp3.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
-        /// </summary>
-        internal static System.Drawing.Bitmap wp5271636_seven_movie_wallpapers {
-            get {
-                object obj = ResourceManager.GetObject("wp5271636-seven-movie-wallpapers", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
